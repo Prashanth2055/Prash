5034e89f0a4e3e9911214349ff12ee856ae618be
Prashanth G <prashanthgun1998@gmail.com>
2025-03-18 19:19:50 +0530

    added

--- project/settings.py
+++ project/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
 
 AUTHENTICATION_BACKENDS = ( 'django.contrib.auth.backends.ModelBackend', 'allauth.account.auth_backends.AuthenticationBackend', )
 
-SITE_ID = 3
+SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 
