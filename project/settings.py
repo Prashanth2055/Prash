a4aa0f8978f5544f92584ffc8b0c01fc5eaeaead
Prashanth G <prashanthgun1998@gmail.com>
2025-05-21 22:40:36 +0530

    Initial commit

--- project/settings.py
+++ project/settings.py
@@ -98,8 +98,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
 
 
